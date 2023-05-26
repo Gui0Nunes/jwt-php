5d2b861fb652c8d60f7a57da5d3213cc7a14050c
Guilherme Nunes <guillhermmme@gmail.com>
2023-05-25 21:57:43 -0300

    Refatoração e correções

--- assets/jwt/jwt.php
+++ assets/jwt/jwt.php
@@ -3,14 +3,16 @@
 /*
 * JWT para encriptografar as chaves 
 * Key para decriptografar as chaves
-*
 */
 use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key;
 
 require "../../vendor/autoload.php";
 
-//chave da criptografia
+/*
+*chave da criptografia
+*orientado guardar em .env
+*/
 $key = hash('sha256','Minha_chave_secreta');
 
 //data criação token
@@ -26,7 +28,6 @@ $exp = strtotime("2023-05-26 20:08:00");
 * status para nivel de permissões, 
 * iat data de criação da chave e 
 * exp data de expiração da chave
-*
 */
 $payload = [
     'id' => '0001',
@@ -39,7 +40,6 @@ $payload = [
 /*
 * X-Forwarded-For ajuda você a identificar o endereço IP de um cliente 
 * quando usar um balanceador de carga HTTP ou HTTPS
-*
 */
 $headers = [
     'x-forwarded-for' => 'guilhermenunes.com.br'
@@ -48,29 +48,23 @@ $headers = [
 // Codificar cabeçalhos na string JWT
 $jwt = JWT::encode($payload, $key, 'HS256', null, $headers);
 
-// $decoded = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+// Decodificando JWT
 $decoded = JWT::decode($jwt, new Key($key,'HS256'));
-// $decoded = json_encode($decoded);
 
-//print do JWT codificado
-echo "<strong> Print do JWT codificado:</strong><br>";
-echo $jwt ;
+//Print na tela com as informações do exemplo
+echo "<strong>Exemplo simples, backend PHP com biblioteca JWT</strong><br><br>";
+
+//print do JWT
+echo "<strong> Print do JWT:</strong><br>";
+echo $jwt."<br><br>" ;
 
 //print decodificado do conteúdo
-echo "<br><br><strong> Print do conteúdo no JWT decodificado:</strong><br>";
-print_r($decoded);
-
+echo "<strong> Print do conteúdo no JWT decodificado:</strong><br>";
+echo $json_decoded = json_encode($decoded)."<br>";
 
 //Data de criação do token
-echo "<hr>";
-$created_at_key2 = date("d/m/Y H:i:s", $created_at_key);
-echo "<strong>Criado em:</strong><br>".$created_at_key2;
-
+$created_at_key2 = date("Y-m-d H:i:s", $created_at_key);
 //Data de expiração do token
-echo "<hr>";
-$exp2 = date("d/m/Y H:i:s", $exp);
-echo "<strong>Expira em:</strong><br>".$exp2;
-
-//orientado guardar em .env
-echo "<hr>";
-echo "<strong>Chave secreta:</strong><br>".$key;
+$exp2 = date("Y-m-d H:i:s", $exp);
+//print das informações de criação, expiração e chave de criptografia
+echo $payload_infos = json_encode(array('Criado em'=>$created_at_key2,'Expira em'=>$exp2,'Key Utilizada:'=>$key));
